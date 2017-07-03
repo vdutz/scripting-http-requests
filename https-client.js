ea4754d0ba4b68296ba1dd15bcafd8ba4a5eef10
Vlad Duta <vlad.du09@gmail.com>
2017-07-03 14:49:52 +0000

    Added code that reads stream from a url and prints to console

---
+++ https-client.js
@@ -0,0 +1,27 @@
+var https = require('https');
+
+function getAndPrintHTMLChunks () {
+
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  /* Add your code here */
+
+  https.get(requestOptions, function(response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function(data) {
+      console.log(data.toString() + "\n")
+    })
+
+    response.on('end', function() {
+      console.log('Response stream complete.')
+    })
+  })
+
+}
+
+getAndPrintHTMLChunks()
